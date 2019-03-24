3f46c7c9dee85b4cebcfda5a81332d04a904ad46
shin-kinoshita <kinoshita116@gmail.com>
2019-03-25 01:00:58 +0900

    modify whale to move

--- src/renderer/views/containers/WhaleSegment.jsx
+++ src/renderer/views/containers/WhaleSegment.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Spring } from 'react-spring/renderprops'
 
 import Whale from '../components/Whale';
 import style from './WhaleSegment.css';
 import { startTimer, stopTimer } from '../../actions';
 import { STOP_MODE } from '../../reducers/mode';
 
+const step = 10;
 class WhaleSegment extends React.Component {
   constructor(props) {
     super(props);
     this.onPressStart = this.onPressStart.bind(this);
     this.onPressStop = this.onPressStop.bind(this);
+    this.state = {
+      positionX: 30,
+      positionY: 40,
+      direction: 1,
+    }
   }
 
   onPressStart() {
@@ -23,12 +30,24 @@ class WhaleSegment extends React.Component {
 
   render() {
     const { mode } = this.props;
+    const { direction, positionX, positionY } = this.state;
     return (
-      <div className={style.whale}>
-        <Whale
-               onPress={mode === STOP_MODE ? this.onPressStart : this.onPressStop}
-        />
-      </div>
+      <Spring from={{rate: 0}}
+              to={{rate: 1}}
+              reset={true}
+              onRest={() => {
+                this.setState({
+                  positionX: this.state.positionX,
+                  positionY: this.state.positionY + step,
+                });
+              }}
+      >
+        {props => (
+          <div className={style.whale} style={{bottom: positionX + 0 * direction, right: positionY + props.rate * step * direction}}>
+            <Whale onPress={mode === STOP_MODE ? this.onPressStart : this.onPressStop}/>
+          </div>
+        )}
+      </Spring>
     );
   }
 }
