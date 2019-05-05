59260b0d3ae54a389f948cad7b4a1ea60e50ed92
shin-kinoshita <kinoshita116@gmail.com>
2019-05-05 16:04:37 +0900

    refactor

--- src/renderer/views/containers/WhaleSegment.jsx
+++ src/renderer/views/containers/WhaleSegment.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Spring } from 'react-spring/renderprops'
+import { Spring, animated } from 'react-spring/renderprops'
 
 import Whale from '../components/Whale';
 import style from './WhaleSegment.css';
@@ -8,16 +8,16 @@ import { generateBubble, startTimer, stopTimer } from '../../actions';
 import { MEASURING_MODE, STOP_MODE } from '../../reducers/mode';
 
 const step = 10;
-const maxY = 390;
-const minY = 220;
+const maxX = 390;
+const minX = 220;
 class WhaleSegment extends React.Component {
   constructor(props) {
     super(props);
     this.onPressStart = this.onPressStart.bind(this);
     this.onPressStop = this.onPressStop.bind(this);
     this.state = {
-      posX: 30,
-      posY: 220,
+      posX: 220,
+      posY: 30,
       direction: 1,
     }
   }
@@ -32,7 +32,7 @@ class WhaleSegment extends React.Component {
 
   updateDest(posX, posY) {
     const { direction } = this.state;
-    if ((maxY - posY) * direction > 0 || (minY - posY) * direction > 0) {
+    if ((maxX - posX) * direction > 0 || (minX - posX) * direction > 0) {
       this.setState({
         posX,
         posY,
@@ -50,17 +50,18 @@ class WhaleSegment extends React.Component {
   render() {
     const { mode } = this.props;
     const { direction, posX, posY } = this.state;
-    const dispY = mode === MEASURING_MODE ? step * direction : 0;
+    const dispX = mode === MEASURING_MODE ? step * direction : 0;
     return (
       <Spring from={{rate: 0}}
               to={{rate: 1}}
               reset={true}
-              onRest={() => this.updateDest(posX, posY + dispY)}
+              onRest={() => this.updateDest(posX + dispX, posY)}
       >
         {props => (
-          <div className={style.whale} style={{bottom: posX + 0 * direction, right: posY + props.rate * dispY}}>
+          <animated.div className={style.whale}
+                        style={{right: posX + props.rate * dispX, bottom: posY}}>
             <Whale direction={direction} onPress={mode === STOP_MODE ? this.onPressStart : this.onPressStop}/>
-          </div>
+          </animated.div>
         )}
       </Spring>
     );
